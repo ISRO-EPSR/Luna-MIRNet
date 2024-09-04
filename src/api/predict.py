d44394483f06ca29bdcbde976abfb84e116ea31b
nilesh <contact.nileshy@gmail.com>
2024-09-05 03:33:34 +0530

    nothing

--- src/api/predict.py
+++ src/api/predict.py
@@ -1,18 +1,17 @@
 from src.data_processing.preprocess import preprocess_image
-from src.models.mirnet import enhance_image_with_mirnet
-from src.data_processing.postprocess import postprocess_image
+from src.models.model import enhance_image
 from pathlib import Path
 
 def process_image(file_path):
-    """Process the image by applying preprocessing, enhancement, and postprocessing."""
+    """Process the image by applying preprocessing and enhancement."""
+    # Convert file_path to a Path object if it is not already one
+    file_path = Path(file_path)
+    
     # Apply preprocessing
     preprocessed_path = preprocess_image(file_path)
     
-    # Apply MIRNet enhancement
-    enhanced_path = enhance_image_with_mirnet(preprocessed_path)
+    # Apply image enhancement
+    enhanced_path = enhance_image(preprocessed_path)
     
-    # Apply postprocessing
-    postprocessed_path = postprocess_image(enhanced_path)
-    
-    # Return the path of the postprocessed image
-    return postprocessed_path
+    # Return the path of the enhanced image
+    return enhanced_path
